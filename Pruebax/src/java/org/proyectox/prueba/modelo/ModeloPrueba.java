83f99ca4a74996cb844b5f33486168bb3226d1da
julioortizrivas <julio.cesar.5025@gmail.com>
2019-10-21 14:51:59 -0500

    Ejemplo

--- Pruebax/src/java/org/proyectox/prueba/modelo/ModeloPrueba.java
+++ Pruebax/src/java/org/proyectox/prueba/modelo/ModeloPrueba.java
@@ -10,5 +10,23 @@ package org.proyectox.prueba.modelo;
  * @author mfab
  */
 public class ModeloPrueba {
-    
+	private Double moneda;
+	private Double cantidad;
+	private Double resultado;
+	 
+	public void setMoneda(Double moneda) {
+	  this.moneda = moneda;
+	}
+	public void setCantidad(Double cantidad) {
+	  this.cantidad = cantidad;
+	}
+	public Double getResultado() {
+	  return resultado;
+	}
+	public void convetirDolarAPeso() {
+	  resultado = cantidad * moneda;
+	}
+	public void convertirPesoADolar() {
+	  resultado = cantidad * moneda;
+	}
 }
